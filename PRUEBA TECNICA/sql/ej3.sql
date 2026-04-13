31f1e2a1e4cc70efad58b07a8524baf4ea928118
almanzasantiago26-hash <almanzasantiago26@gmail.com>
2026-04-13 11:12:51 -0500

    Cambio base de datos

--- PRUEBA TECNICA/sql/ej3.sql
+++ PRUEBA TECNICA/sql/ej3.sql
@@ -8,13 +8,3 @@ LEFT JOIN pedidos p ON pr.id_producto = p.id_producto
 WHERE p.id_producto IS NULL
   AND pr.stock > 0;
 
--- Opción B: NOT EXISTS (equivalente)
--- SELECT
---     pr.nombre,
---     pr.categoria,
---     pr.precio
--- FROM productos pr
--- WHERE pr.stock > 0
---   AND NOT EXISTS (
---       SELECT 1 FROM pedidos p WHERE p.id_producto = pr.id_producto
---   );
